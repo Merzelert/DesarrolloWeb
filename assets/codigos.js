aa5618d706d2ce68f98670f1ae720616d753cba3
Erik Retana <erik.retana.lopez@gmail.com>
2022-05-31 15:25:12 -0500

    Commit inicial

---
+++ assets/codigos.js
@@ -0,0 +1,10 @@
+console.log("Aprendiendo a invocar codigo JS desde HTML");
+
+let nombre = "Erik";
+let apellido = "Retana";
+
+let saludo = "Hola mi nombre es " + nombre + " " + apellido + ".";
+console.log(saludo);
+
+let saludo2 = `Hola mi nombre es ${nombre} ${apellido}.`;
+console.log(saludo2);
